8fb15bd7d23fe448949c248b4127861edb1467ab
QWilson25 <129434589+QWilson25@users.noreply.github.com>
2023-04-29 17:50:34 -0500

    Update account.cpp

--- account.cpp
+++ account.cpp
@@ -1,40 +1,32 @@
-// classes example
 #include <iostream>
 #include <string>
 #include<vector>
 #include "account.h";
 
 using namespace std;
-   Account(){
-    }
-    // Getters 
-    string Account::getShipping(){
-        return Shipping;
-    }
-    string Account::getBilling(){
-       return Billing; 
-    }
+Account::Account() {
+}
+// Getters 
+string Account::getShipping() {
+    return Shipping;
+}
+string Account::getBilling() {
+    return Billing;
+}
 
-    string Account::getOrderInfo(){
-        return Billing; 
-    }
-    string Account::getPayment(){
-        return Payment; 
-    }
-    string Account::setShipping(string address1){
-        this->Shipping = Shipping; 
-    }
-    string Account::setBilling(string address2){
-        this->Billing = Billing; 
+string Account::getOrderInfo() {
+    return Billing;
+}
+string Account::getPayment() {
+    return Payment;
+}
+string Account::setShipping(string address1) {
+    this->Shipping = Shipping;
+}
+string Account::setBilling(string address2) {
+    this->Billing = Billing;
 
-    }
-    string Account::setPayment(string card){
-        this->Payment = Payment; 
-    } 
-    
-     // Setters 
-    void Account::deleteAccount(){
-        return 
-        
-    }
-    
+}
+string Account::setPayment(string card) {
+    this->Payment = Payment;
+}
