ccda59ffec1a0077fcc5c9b1df2a0c7584b7d5e5
QWilson25 <129434589+QWilson25@users.noreply.github.com>
2023-04-30 18:40:02 -0500

    Update account.h

--- account.h
+++ account.h
@@ -9,26 +9,24 @@ using std::string;
 class Account
 {
 private:
-    string Shipping = "", Billing = "", OrderInfo = "", Payment = "";
+    string Shipping = "", Billing = "", Payment = "";
 
 public:
     Account();
-    Account(string Shipping, string Billing, string OrderInfo, string Payment)
+    Account(string Shipping, string Billing, string Payment)
     {
         this->Shipping = Shipping;
         this->Billing = Billing;
-        this->OrderInfo = OrderInfo;
         this->Payment = Payment;
     };
 
     // getters
     string getBilling();
-    string getOrderInfo();
     string getShipping();
     string getPayment();
-    string setShipping(string shipping);
-    string setBilling(string billing);
-    string setPayment(string Payment);
+    void setShipping(string shipping);
+    void setBilling(string billing);
+    void setPayment(string Payment);
 
 };
 
